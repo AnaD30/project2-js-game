1f86c678fbf8539cdb36f0f0c72135141bdf74e7
Ana Duler <ana.duler93@gmail.com>
2024-03-27 10:55:26 +0000

    Reconstructing the whole structure

--- README.md
+++ README.md
@@ -3,23 +3,44 @@
 
 # The game of ROCK, PAPER, SCISSORS!
 
-The game Rock,paper,acissora is a static website about that shoes the user a simle game of rock, paper,scissors against the computer.
+The game Rock,paper,sscissors,lizard, spock is a static website about that shows the user a simple,interactive game of rock, paper,scissors,lizard,spock against the computer.
 
 ## Table of Contents
 
 ## User Experience
 
-The goal of this website 
+The goal of this website to bring to the user a interactive game. That also make the user hooked for more fun.
 
 ## Existing Features:
 
-### Navigation Bar
+### Menu :
+
+Fully responsive menu with the classic version of Rock, paper,scissor that lets the user experience a game simple but enjoyable no matter the age.
+The other version is a new version of Rock, paper,scissors, lizard, Spock.Inspired by the series The Big Bang Theory. Where here origin lies.
 
 # Presentation of the main part:
+### Game
+Here is the original version of Rock,paper,scissors.
+A simple but interactive site with the game of Rock, paper, scissors. Urging the user to try the fun himself.
+
+### New Game
+
+The new version of Spock with added additional difficulty of two more options: lizard and Spock. Inspired by the American series of The Big Bang Theory. Even more challenging and engaging for the user to try.
+
+
+### Rules
+#### The original version and the explanation for use:
+The rock is a closed fist; paper is a flat hand with fingers and thumb extended and the palm facing downward; and scissors is a fist with the index and middle fingers fully extended toward the opposing player. Rock wins against scissors; paper wins against rock; and scissors wins against paper.
+
+#### The new version with lizard,spock:
+The rules are like those of the basic Rock Paper Scissors game, but with 2 extra movements. Scissors cuts paper, paper covers rock, rock crushes lizard, lizard poisons Spock, Spock smashes scissors, scissors decapitates lizard, lizard eats paper, paper disproves Spock, Spock vaporizes rock, and rock crushes scissors.
 
 ## The landing page image
 
 ### Validator Testing
+CSS
+HTML
+JAVASCRIPT 
 
 #  Accessibility 
 
--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,8 @@
 /**
  * Declare constants for Dom elements
  * and possible choices
- */
+ * 
+
 const choices = ["rock", "paper", "scissors,lizard,spock"];
 const playerDisplay = document.getElementById("playerDisplay");
 const computerDisplay = document.getElementById("computerDisplay");
@@ -13,96 +14,96 @@ const logo = document.getElementById(logo)
 const rockButton = document.getElementById('rock');
 const paperButton = document.getElementById('paper');
 const scissorsButton = document.getElementById('scissors');
+const lizardButton = document.getElementById('lizard');
+const spockButton = document.getElementById('spock');
 
 rockButton.addEventListener('click', playGame);
 paperButton.addEventListener('click', playGame);
 scissorsButton.addEventListener('click', playGame);
+lizardButton.addEventListener('click', playGame);
+spockButton.addEventListener('click', playGame);
 
 
 let playerScore = 0;
 let computerScore = 0;
 
+*/
 /** menu area */
-// Get the modal
-const modal = document.getElementById("myModal");
-const btn = document.getElementsById("myBtn--blue");
-const span = document.getElementsByClassName("close")[0];
+const toggleBtn = document.querySelector('toggle_btn');
+const toggleBtnIcon = document.querySelector('toggle_btn i');
+const dropDownMenu = document.querySelector('dropdown_menu');
 
+toggleBtn.onclick = function () {
+    dropDownMneu.classList.toggle('open')
+    const isOpen = dropDownMenu.classList.contains('open')
 
+    toggleBtnIcon.classList = isOpen
+    ? 'fa-solid fa-xmark'
+    :'fa-solid fa-bars'
 
-// When the user clicks the button, open the modal 
-btn.addEventListener('click', myFunction);
-function myFunction() {
-    modal.style.display = "block";
-    
-  }
-btnClose.addEventListener('click',btnClose);
-function () {
-   
 }
- // When the user clicks on <span> (x), close the modal 
- span.onclick = function() {
-    modal.style.display = "none";
-  }
-
-// When the user clicks anywhere outside of the modal, close it
-  window.onclick = function(event) {
-    if (event.target == modal) {
-      modal.style.display = "none";
-    }
-  }
 
 /**
  * Gets the current score from the DOM and 
  * gets the current tally of incorrect answers from the DOM 
  *
- */
+
 
 function playGame(event) {
     console.log(event);
     let playerChoice = event.target.id;
     console.log(playerChoice);
+}
     const computerChoice = choices[Math.floor(Math.random() * 5)];
     let result = "";
-
+*/
     /**
      * Gets the current tally of incorrect answers from the DOM 
-     */
-
+     
     if (playerChoice === computerChoice) {
         result = "IT'S A TIE!";
     } else {
         switch (playerChoice) {
             case "rock":
-                result = (computerChoice === "scissors") ? "YOU WIN,😎!" : "Want to Quit!";
+                result = (computerChoice === "scissors") ? "YOU WIN!" : "Want to Quit!";
                 break;
             case "paper":
-                result = (computerChoice === "rock") ? "YOU WIN!😎" : "Want to Quit!";
+                result = (computerChoice === "rock") ? "YOU WIN!" : "Want to Quit!";
                 break;
             case "scissors":
-                result = (computerChoice === "paper") ? "YOU WIN!😎" : "Want to Quit!";
+                result = (computerChoice === "paper") ? "YOU WIN!" : "Want to Quit!";
+                break;
+
+            case "lizard":
+                result = (computerChoice === "rock") ? "YOU WIN!" : "Want to Quit!";
+                break;
+            case "spock":
+                result = (computerChoice === "lizard") ? "YOU WIN!" : "Want to Quit!";
                 break;
         }
-    }
-    /**
-     * main game function.
-     */
-    playerDisplay.textContent = `PLAYER: ${playerChoice}`;
-    computerDisplay.textContent = `Computer: ${computerChoice}`;
-    resultDisplay.textContent = result;
 
-    resultDisplay.classList.remove("darkblueText", "lightblueText");
+    }*/
+        /**
+         * main game function.
+        
+        playerDisplay.textContent = `PLAYER: ${playerChoice}`;
+        computerDisplay.textContent = `Computer: ${computerChoice}`;
+        resultDisplay.textContent = result;
 
-    switch (result) {
-        case "YOU WIN!":
-            resultDisplay.classList.add("darkblueText");
-            playerScore++;
-            playerScoreDisplay.textContent = playerScore;
-            break;
-        case "Want to QUIT?":
-            resultDisplay.classList.add("lightblueText");
-            computerScore++;
-            computerScoreDisplay.textContent = computerScore;
-            break;
-    }
-}
+        resultDisplay.classList.remove("darkblueText", "lightblueText");
+
+        switch (result) {
+            case "YOU WIN!":
+                resultDisplay.classList.add("darkblueText");
+                playerScore++;
+                playerScoreDisplay.textContent = playerScore;
+                break;
+            case "Want to QUIT?":
+                resultDisplay.classList.add("lightblueText");
+                computerScore++;
+                computerScoreDisplay.textContent = computerScore;
+                break;
+        }
+
+
+ */
