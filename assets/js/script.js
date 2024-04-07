0c05e3f5f5468353c09512bc03da9776b7e4362d
Ana Duler <ana.duler93@gmail.com>
2024-04-07 11:42:14 +0000

    additional functions for the modal and adding the modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,11 +2,14 @@
 
 
 /**Menu area responsiveness coneccted to Javascript*/
-// refering to classicGame
-let playGameBtn = document.getElementById("play-game");
-let playGame2Btn = document.getElementById("play-game2");
 let container = document.getElementById("game-container");
-let rulesButton = document.getElementById("rules");
+let playGameBtn;
+let playGame2Btn;
+let rulesButton;
+let quitModal = document.getElementById("quitModal");
+let quitModalCloseBtn = document.getElementById("quit-modal-close");
+let quitModalYesBtn = document.getElementById("quit-yes");
+let quitModalNoBtn = document.getElementById("quit-no");
 let choices;
 let playerDisplay;
 let computerDisplay;
@@ -41,7 +44,7 @@ let classicGame = `<div class="roshambo">
   Computer Score:<span id="computerScoreDisplay">0</span>
   <div id="close-container">
     <div class="closegame">
-      <button class="close-button">&times</button>
+      <button id="game-close" class="close-button">&times</button>
     </div>
   </div>
 </div>
@@ -75,39 +78,43 @@ let newGame = `<div class="roshambo2">
   Computer Score:<span id="computerScoreDisplay">0</span>
   <div id="close-container2">
     <div class="closegame2">
-      <button class="close-button2">&times</button>
+      <button id="game-close" class="close-button2">&times</button>
     </div>
   </div>
 </div>
 </div>`;
 
 let menu = `<div>
-<button id="play-game">Play Game</button>
+<button class="style-button" id="play-game">Play Game</button>
 </div>
 <div>
-<button id="play-game2">Play Game 2</button>
+<button class="style-button-2" id="play-game2">Play Game 2</button>
 </div>
 <div>
-<button id="rules">Rules of the game</button> 
+<button class="style-button-rules" id="rules">Rules of the game</button> 
 </div>`;
 
 
-playGameBtn.addEventListener('click', insurtGame);
-playGame2Btn.addEventListener('click', insurtGame2);
-rulesButton.addEventListener('click',openEndRulesModal);
+
 
 
 //insurting the classicclassical  version of the game
 function insurtGame() {
-    container.innerHTML = classicGame;
-    setUpClassicGame()
+  container.innerHTML = classicGame;
+  setUpClassicGame()
 }
+
 
 //insurting the new version of the game
 function insurtGame2() {
-    container.innerHTML = newGame;
-    setUpClassicGame()
-    setUpNewGame()
+  container.innerHTML = newGame;
+  setUpClassicGame()
+  setUpNewGame()
+}
+
+//insurting rules of the game 
+function insurtRules() {
+  openEndRulesModal()
 }
 
 /**
@@ -115,22 +122,23 @@ function insurtGame2() {
  * and possible choices
  * */
 function setUpClassicGame() {
-    choices = ["rock", "paper", "scissors"];
-    playerDisplay = document.getElementById("playerDisplay");
-    computerDisplay = document.getElementById("computerDisplay");
-    resultDisplay = document.getElementById("resultDisplay");
-    playerScoreDisplay = document.getElementById("playerScoreDisplay");
-    computerScoreDisplay = document.getElementById("computerScoreDisplay");
+  choices = ["rock", "paper", "scissors"];
+  playerDisplay = document.getElementById("playerDisplay");
+  computerDisplay = document.getElementById("computerDisplay");
+  resultDisplay = document.getElementById("resultDisplay");
+  playerScoreDisplay = document.getElementById("playerScoreDisplay");
+  computerScoreDisplay = document.getElementById("computerScoreDisplay");
 
-    logo = document.getElementById("logo");
+  logo = document.getElementById("logo");
 
-    const rockButton = document.getElementById('rock');
-    const paperButton = document.getElementById('paper');
-    const scissorsButton = document.getElementById('scissors');
-
-    rockButton.addEventListener('click', playGame);
-    paperButton.addEventListener('click', playGame);
-    scissorsButton.addEventListener('click', playGame);
+  const rockButton = document.getElementById('rock');
+  const paperButton = document.getElementById('paper');
+  const scissorsButton = document.getElementById('scissors');
+  const closeGameBtn = document.getElementById("game-close");
+  rockButton.addEventListener('click', playGame);
+  paperButton.addEventListener('click', playGame);
+  scissorsButton.addEventListener('click', playGame);
+  closeGameBtn.addEventListener('click', openQuitModal);
 
 }
 
@@ -138,12 +146,12 @@ function setUpClassicGame() {
  * second part of the choices,connected to the newGame
  */
 function setUpNewGame() {
-    choices = ["rock", "paper", "scissors", "lizard", "spock"];
-    const lizardButton = document.getElementById('lizard');
-    const spockButton = document.getElementById('spock');
+  choices = ["rock", "paper", "scissors", "lizard", "spock"];
+  const lizardButton = document.getElementById('lizard');
+  const spockButton = document.getElementById('spock');
 
-    lizardButton.addEventListener('click', playGame);
-    spockButton.addEventListener('click', playGame);
+  lizardButton.addEventListener('click', playGame);
+  spockButton.addEventListener('click', playGame);
 }
 
 /**
@@ -155,40 +163,32 @@ let endRulesModal = document.getElementById("endRulesModal");
 let modalCloseRules = document.getElementsByClassName("modalCloseRules")[0];
 
 //when the user click on the rules of the games
-function openEndRulesModal(){
+function openEndRulesModal() {
   endRulesModal.style.display = "block";
 }
 
-function closeEndRulesmodal(){
+function closeEndRulesmodal() {
   endRulesModal.style.display = "none";
-
-  // go back to the main menu
-  goToMainMenu()
 }
 
 // When the user clicks on <span> (x), close the modal
-endRulesModal.addEventListener('click',closeEndRulesmodal);
-// When the user clicks anywhere outside of the modal, close it
-window.addEventListener('click', () => {
-  if (event.target == endRulesModal) {
-      endRulesModal.style.display = "none";
-  }
-})
+endRulesModal.addEventListener('click', closeEndRulesmodal);
+
 
 /**
  * Gets the score of the first game from the DOM and 
  * gets the first tally of incorrect answers from the DOM 
  **/
 function playGame(event) {
-    console.log(event);
-    let playerChoice = event.target.id;
-    console.log(playerChoice);
-    generateComputerChoice(choices, playerChoice);
+  console.log(event);
+  let playerChoice = event.target.id;
+  console.log(playerChoice);
+  generateComputerChoice(choices, playerChoice);
 }
 
 function generateComputerChoice(choices, playerChoice) {
-    let computerChoice = choices[Math.floor(Math.random() * choices.length)];
-    calculateWinner(computerChoice, playerChoice);
+  let computerChoice = choices[Math.floor(Math.random() * choices.length)];
+  calculateWinner(computerChoice, playerChoice);
 
 }
 
@@ -196,81 +196,81 @@ function generateComputerChoice(choices, playerChoice) {
  * Gets the current tally of incorrect answers from the DOM 
  */
 function calculateWinner(computerChoice, playerChoice) {
-    if (playerChoice === computerChoice) {
-        result = "IT'S A TIE!";
-    } else {
-        switch (playerChoice) {
-            case "rock":
-                result = (computerChoice === "scissors" || computerChoice === "lizard") ? "YOU WIN!" : "Want to Quit!";
-                break;
-            case "paper":
-                result = (computerChoice === "rock" || computerChoice === "spock") ? "YOU WIN!" : "Want to Quit!";
-                break;
-            case "scissors":
-                result = (computerChoice === "paper" || computerChoice === "lizard") ? "YOU WIN!" : "Want to Quit!";
-                break;
-            case "lizard":
-                result = (computerChoice === "paper" || computerChoice === "spock") ? "YOU WIN!" : "Want to Quit!";
-                break;
-            case "spock":
-                result = (computerChoice === "scissors" || computerChoice === "rock") ? "YOU WIN!" : "Want to Quit!";
-                break;
-        }
+  if (playerChoice === computerChoice) {
+    result = "IT'S A TIE!";
+  } else {
+    switch (playerChoice) {
+      case "rock":
+        result = (computerChoice === "scissors" || computerChoice === "lizard") ? "YOU WIN!" : "Want to Quit!";
+        break;
+      case "paper":
+        result = (computerChoice === "rock" || computerChoice === "spock") ? "YOU WIN!" : "Want to Quit!";
+        break;
+      case "scissors":
+        result = (computerChoice === "paper" || computerChoice === "lizard") ? "YOU WIN!" : "Want to Quit!";
+        break;
+      case "lizard":
+        result = (computerChoice === "paper" || computerChoice === "spock") ? "YOU WIN!" : "Want to Quit!";
+        break;
+      case "spock":
+        result = (computerChoice === "scissors" || computerChoice === "rock") ? "YOU WIN!" : "Want to Quit!";
+        break;
     }
-    updateScores(result, computerChoice, playerChoice)
+  }
+  updateScores(result, computerChoice, playerChoice)
 }
 
 /**
  * main game function
  */
 function updateScores(result, computerChoice, playerChoice) {
-    playerDisplay.textContent = `PLAYER: ${playerChoice}`;
-    computerDisplay.textContent = `Computer: ${computerChoice}`;
-    resultDisplay.textContent = result;
+  playerDisplay.textContent = `PLAYER: ${playerChoice}`;
+  computerDisplay.textContent = `Computer: ${computerChoice}`;
+  resultDisplay.textContent = result;
 
-    resultDisplay.classList.remove("darkvioletText", "lightvioletText");
+  resultDisplay.classList.remove("darkvioletText", "lightvioletText");
 
-    switch (result) {
-        case "YOU WIN!":
-            resultDisplay.classList.add("darkvioletText");
-            playerScore++;
-            playerScoreDisplay.textContent = playerScore;
-            break;
-        case "Want to Quit!":
-            console.log("updateComputerScore");
-            console.log(computerScore);
-            resultDisplay.classList.add("lightvioletText");
-            computerScore++;
-            console.log(computerScore);
-            computerScoreDisplay.textContent = computerScore;
-            break;
-    }
-    checkForOverallWinner()
+  switch (result) {
+    case "YOU WIN!":
+      resultDisplay.classList.add("darkvioletText");
+      playerScore++;
+      playerScoreDisplay.textContent = playerScore;
+      break;
+    case "Want to Quit!":
+      console.log("updateComputerScore");
+      console.log(computerScore);
+      resultDisplay.classList.add("lightvioletText");
+      computerScore++;
+      console.log(computerScore);
+      computerScoreDisplay.textContent = computerScore;
+      break;
+  }
+  checkForOverallWinner()
 }
 
 function checkForOverallWinner() {
-    let winningScore = 5;
-    if (playerScore >= winningScore) {
-        //add player wins pop message 
-        console.log("You won!");
-       
-        playerScoreModal.innerText = playerScore;
-        computerScoreModal.innerText = computerScore;
-        modalMessage.innerText = "You won!";
-        openEndGameModal()
-    } else if (computerScore >= winningScore) {
-        //add player loses haha pop message
-        console.log("You lost,want to leave?");
+  let winningScore = 5;
+  if (playerScore >= winningScore) {
+    //add player wins pop message 
+    console.log("You won!");
 
-        playerScoreModal.innerText = playerScore;
-        computerScoreModal.innerText = computerScore;
-        modalMessage.innerText = "You lost,want to leave?";
-        openEndGameModal()
+    playerScoreModal.innerText = playerScore;
+    computerScoreModal.innerText = computerScore;
+    modalMessage.innerText = "You won!";
+    openEndGameModal()
+  } else if (computerScore >= winningScore) {
+    //add player loses haha pop message
+    console.log("You lost,want to leave?");
 
-    } else {
-        //when nobody won overall
-        console.log("play-next-round");
-    }
+    playerScoreModal.innerText = playerScore;
+    computerScoreModal.innerText = computerScore;
+    modalMessage.innerText = "You lost,want to leave?";
+    openEndGameModal()
+
+  } else {
+    //when nobody won overall
+    console.log("play-next-round");
+  }
 }
 
 // Get the modal for the classical game and the new version of the game
@@ -279,27 +279,62 @@ let playerScoreModal = document.getElementById("modal-player-score");
 let computerScoreModal = document.getElementById("modal-computer-score");
 let modalMessage = document.getElementById("modal-message");
 // Get the <span> element that closes the modal
-let modalClose = document.getElementsByClassName("modalClose")[0];
+let modalClose = document.getElementsByClassName("modalClose")[1];
 
 // When the user reaches the end of the game, open the modal 
 function openEndGameModal() {
-    endGameModal.style.display = "block";
+  endGameModal.style.display = "block";
 }
 
 function closeEndGameModal(event) {
+  if (event.target == quitModalYesBtn) {
+    quitModal.style.display = "none";
+  } else {
     endGameModal.style.display = "none";
+  }
 
-    // reset the players scores
-    
-    // go back to the main menu
-    goToMainMenu()
+  // reset the players scores
+  playerScore = 0;
+  computerScore = 0;
+
+  // go back to the main menu
+  goToMainMenu()
+}
+
+//get you to zhe main menu and displays the buttons
+function goToMainMenu() {
+  container.innerHTML = menu;
+  playGameBtn = document.getElementById("play-game");
+  playGame2Btn = document.getElementById("play-game2");
+  rulesButton = document.getElementById("rules");
+  playGameBtn.addEventListener('click', insurtGame);
+  playGame2Btn.addEventListener('click', insurtGame2);
+  rulesButton.addEventListener('click', insurtRules);
+}
+
+quitModalCloseBtn.addEventListener('click', closeQuitModal);
+quitModalNoBtn.addEventListener('click', closeQuitModal);
+quitModalYesBtn.addEventListener('click', closeEndGameModal);
+
+function openQuitModal() {
+  quitModal.style.display = "block";
+}
+
+function closeQuitModal() {
+  quitModal.style.display = "none";
 }
 
 // When the user clicks on <span> (x), close the modal
 modalClose.addEventListener('click', closeEndGameModal)
 // When the user clicks anywhere outside of the modal, close it
 window.addEventListener('click', () => {
-    if (event.target == endGameModal) {
-        endGameModal.style.display = "none";
-    }
+  if (event.target == endGameModal) {
+    endGameModal.style.display = "none";
+  } else if (event.target == quitModal) {
+    quitModal.style.display = "none";
+  } else if (event.target == endRulesModal) {
+    endRulesModal.style.display = "none";
+  }
 })
+
+document.addEventListener('DOMContentLoaded', goToMainMenu);
